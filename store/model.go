dc151a08f369d3f6f9bcd8cbfa983c4b10a2e917
Thomas Fossati <thomas.fossati@nokia.com>
2017-04-25 15:59:54 +0100

    renamed api to model

---
+++ store/model.go
@@ -0,0 +1,43 @@
+package lurkstore
+
+import (
+	"database/sql"
+)
+
+var db *sql.DB
+
+// TODO make backend choice pluggable
+
+func Init(filename string) (err error) {
+	db, err = DbInit(filename)
+	return
+}
+
+// Store a new registration
+// Returns the unique id for the newly created record
+func NewRegistration(csr string, lifetime uint) (string, error) {
+	return DbAddRegistration(db, csr, lifetime)
+}
+
+// Return the Registration record associated to the supplied id, if found
+func GetRegistrationById(id string) (*Registration, error) {
+	return DbGetRegistrationById(db, id)
+}
+
+// Fetch the oldest registration in state "new" (if one exists) and mark it
+// as "work-in-progress"
+func DequeueRegistration() (*Registration, error) {
+	return DbDequeueRegistration(db)
+}
+
+// Mark a work-in-progress as successfully completed
+func UpdateSuccessfulRegistration(id string, certURL string, lifetime uint,
+	ttl string) error {
+	return DbUpdateSuccessfulRegistration(db, id, certURL, lifetime, ttl)
+}
+
+// Mark a work-in-progress as failed
+// TODO control how long the registration is visible
+func UpdateFailedRegistration(id string, errmsg string) error {
+	return DbUpdateFailedRegistration(db, id, errmsg)
+}
