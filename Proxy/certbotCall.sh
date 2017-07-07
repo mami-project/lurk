5b45fe8e9b7da9d3d3262f235a8fa10467bf3552
diegoDAguilar <diego.deaguilarcanellas@telefonica.com>
2017-07-07 12:55:34 +0200

    Update certbotCall.sh

--- Proxy/certbotCall.sh
+++ Proxy/certbotCall.sh
@@ -3,7 +3,8 @@ domainName=$1
 cd /root/certbot/certbot
 a=" --csr ../../tmpCsr --agree-tos --renew-by-default -d $domainName"
 b=" --server http://172.17.0.4:4000/directory --webroot -w /var/www/$domainName/html"
+c=" --cert-path /etc/letsencrypt/live/$domainName/cert.pem --chain-path /etc/letsencrypt/live/$domainName/chain.pem --fullchain-path /etc/letsencrypt/live/$domainName/fullchain.pem "
 
 #./certbot-auto certonly $a $b
 
-python main.py certonly $a $b
+python main.py certonly $a $b $c
